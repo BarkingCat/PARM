a19ad93172cb4dbc751114015e3680b7237ccf25
Aaron Jackson <a.l.jackson85@gmail.com>
2015-04-06 19:22:58 +1000

    Baseline commit

---
+++ PowerShell/Download_AFR_Files.ps1
@@ -0,0 +1,84 @@
+﻿# Add to source control
+# Add description
+Clear-Host
+
+# Download this file
+# http://www.afr.com/rw/AFR/Web/Tables/Share_Tables_Daily/2015-03-28/GGsoda150328.csv
+
+[string]$DB_Server = '121.217.16.179' #NVRMND-PC or 121.217.16.179
+[string]$DB_Name = 'PARM_Control'
+[string]$DB_User = 'neo' #set up application username and password
+[string]$DB_Password = 'trust.08'#make account read only, must use procs to change data
+
+try{
+
+# check local path is valid
+
+    # Get RunID
+    $RunID = 101
+
+
+    while ($RunID -le 201)
+    {
+
+        # Query PARM to populate these variables
+        $sql = "EXEC [Control].[GetFileInformation] @RunID = $RunID"
+
+        # Create and open a database connection
+        #$sqlConnection = new-object System.Data.SqlClient.SqlConnection "server=$DB_Server;database=$DB_Name;Integrated Security=sspi"
+        $sqlConnection = new-object System.Data.SqlClient.SqlConnection "server=$DB_Server;database=$DB_Name;User Id=$DB_User;Password=$DB_Password;"
+    
+        $sqlConnection.Open()
+        #Create a command object
+        $sqlCommand = $sqlConnection.CreateCommand()
+        $sqlCommand.CommandText = $sql
+        #Execute the Command
+        $sqlReader = $sqlCommand.ExecuteReader()
+        #Parse the records
+
+        while ($sqlReader.Read()) 
+        {
+            $FileID = $sqlReader["FileID"] 
+            $Url = $sqlReader["SourceURI"] + $sqlReader["FileName"]
+            $LocalPath = $sqlReader["TargetURI"] + $sqlReader["FileName"]
+        }
+
+        # Close the database connection
+        $sqlConnection.Close()
+
+        # Check file exists - if exists delete
+
+        $Wget = New-Object System.Net.WebClient
+        $Wget.DownloadFile($Url, $LocalPath)
+
+        #Check success -- http code?
+        # verify files exists
+
+        Write-Host "$LocalPath downloaded"
+
+        #Update PARM with success
+        $sqlConnection.Open()
+        $sql = "EXEC [Control].[SetDownloadStatus] @RunID = $RunID, @FileID = $FileID, @StatusCode = 2"
+        $sqlCommand = $sqlConnection.CreateCommand()
+        $sqlCommand.CommandText = $sql
+        $sqlCmd = $sqlCommand.ExecuteNonQuery()
+        $sqlConnection.Close()
+
+        $RunID++
+
+    }
+
+}
+catch {
+
+    Write-Host "Error detected. Code exiting.." -backgroundcolor black -foregroundcolor red
+
+    # Output all errors to console
+
+    # Update PARM with Error
+
+    Write-Host $Error -backgroundcolor black -foregroundcolor red
+
+    Write-Host $_.Exception.ToString() -backgroundcolor black -foregroundcolor red
+
+}
