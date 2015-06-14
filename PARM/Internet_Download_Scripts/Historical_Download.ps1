ad27d04c5bb44593f34b0ac0fa717185da9a3488
Aaron Jackson <a.l.jackson85@gmail.com>
2015-06-14 14:33:31 +1000

    Cube progress

---
+++ PARM/Internet_Download_Scripts/Historical_Download.ps1
@@ -0,0 +1,24 @@
+﻿#######
+# Information
+#######
+
+# Version: 0.1
+# Developer: Aaron Jackson
+# Date: 13/06/2015
+# Desc: This script will call the download script in a loop
+
+## TEST
+# .\Historical_Download.ps1
+##
+
+$i = 0
+
+while ($i -le 1)
+{
+    
+    Write-Host "Iteration $i"
+
+	.\Download_AFR_Files.ps1 -System_Name "AFR" -Frequency "D"
+
+	$i++
+}
